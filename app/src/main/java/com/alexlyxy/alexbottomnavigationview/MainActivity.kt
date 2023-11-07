528fcaaae326d58b466aaad9e2251b9e47af9416
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-11-07 19:54:04 +0200

    Last Version 07.11.2023

---
+++ app/src/main/java/com/alexlyxy/alexbottomnavigationview/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.alexlyxy.alexbottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.alexlyxy.alexbottomnavigationview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bNav.selectedItemId = R.id.item3
+        binding.bNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item2", Toast.LENGTH_SHORT).show()
+
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item3", Toast.LENGTH_SHORT).show()
+
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item4", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+            true
+        }
+    }
+}
